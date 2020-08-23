0de3d9b368e5e1851e8488e9def59fea982b17d8
Ericcentrik <enaiman1@gmail.com>
2020-08-23 15:08:30 -0400

    added controllers and middle ware

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const dontenv =require('dotenv')
+const dontenv =require('dotenv');
+const morgan = require('morgan')
+
+//Route Files
+const bootcamps = require('./routes/bootcamps')
 
 // Load env vars
 dontenv.config({path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+// dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+app.use(morgan('dev'))
+}
+
+// Mounts routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,
     console.log(`App listening on ${process.env.NODE_ENV} mode on port ${PORT}!`)
