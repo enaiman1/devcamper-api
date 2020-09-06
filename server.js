b123f9c9cfa09f96bf8494a8924fd6480e36ea78
Ericcentrik <enaiman1@gmail.com>
2020-09-06 14:18:48 -0400

    add more security features

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const morgan = require('morgan');
 const colors = require ('colors');
 const fileupload = require('express-fileupload');
 // const cookieParser = require('cookie-parser');
+const mongoSantize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -36,6 +42,30 @@ app.use(morgan('dev'))
 
 //File uploading
 app.use(fileupload())
+
+//Santize data
+app.use(mongoSantize())
+
+//Set secuirty headers
+app.use(helmet())
+
+//Prevent cross site attacks
+app.use(xss())
+
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 
+})
+
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp())
+
+//Enable CORS
+app.use(cors())
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
